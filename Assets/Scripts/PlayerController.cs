8a6492d7e70bd65f9a5d2c516e3e2af0071763a6
Kevin <shakarax@gmail.com>
2017-02-04 21:06:51 -0800

    fixing conflict

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject hammerPrefab;
+    public Transform hammerSpawn;
+
     void Update()
     {
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
@@ -11,5 +14,25 @@ public class PlayerController : MonoBehaviour
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Throw();
+        }  
+    }
+
+    void Throw()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var hammer = (GameObject)Instantiate(
+            hammerPrefab,
+            hammerSpawn.position,
+            hammerSpawn.rotation);
+
+        // Add velocity to the hammer
+        hammer.GetComponent<Rigidbody>().velocity = hammer.transform.forward * 6;
+
+        // Destroy the hammer after 5 seconds
+        Destroy(hammer, 5.0f);
     }
 }
