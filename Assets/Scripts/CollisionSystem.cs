54c2473f7570f89d8b8c22973d22f04ab98523f1
jsjwooowooo <jsjwooowooo@gmail.com>
2017-02-04 16:52:51 -0800

    Collision system start

---
+++ Assets/Scripts/CollisionSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSystem
+{
+	void OnCollisionEnter ( Collision collision )
+	{
+		Debug.Log( "HIT" );
+	}
+}
--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class SystemManager : MonoBehaviour {
 
 	LifetimeSystem lifetimeSystem;
+	CollisionSystem collisionSystem;
 
 	// Use this for initialization
 	void Start () {
+		collisionSystem = new CollisionSystem();
 		lifetimeSystem = new LifetimeSystem();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		var gameObjects = FindObjectsOfType<GameObject>();
+		collisionSystem.run( gameObjects );
 		lifetimeSystem.run( gameObjects );
 	}
 }
