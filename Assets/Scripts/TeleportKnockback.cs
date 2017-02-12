3a34d685a14c103cc6564516186d8c805940dfa7
Kevin <shakarax@gmail.com>
2017-02-11 20:54:29 -0800

    Adding Teleport Knockback.

---
+++ Assets/Scripts/TeleportKnockback.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Applies an explosion force to all nearby rigidbodies
+public class TelepoerKnockback : MonoBehaviour
+{
+    public float radius = 5.0F;
+    public float power = 10.0F;
+
+    void Start()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+
+        }
+    }
+}
